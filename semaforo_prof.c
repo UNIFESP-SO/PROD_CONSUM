320a35afdcaefc98b715d44980c5ae06cc3c7d80
saxsoares <sax.soares@gmail.com>
2016-03-31 23:50:16 +0000

    atualizado

--- semaforo_prof.c
+++ semaforo_prof.c
@@ -4,22 +4,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define NPROD 2
 #define N 10
-
 int itens[N];
 sem_t vazio; // <- N
 sem_t cheio; // <- 0
 int in = 0,  //posicao p/ inserir no buffer itens
     out= 0;  //posicao p/ remover do buffer itens
-int sem[1];
-sem_t mut;
 
 int produz(void) {
 	return rand()%100;
 }
 
 void insere_item(int item){
+	sleep(1);
 	itens[in] = item;
 }
 
@@ -30,14 +27,11 @@ int remove_item(void){
 void *produtor(void *args) {
 	int item;
 	while (1) {
-		sleep(1);
 		sem_wait(&vazio);
-		sem_wait(&mut);
 		item = produz();
 		insere_item(item);
-		printf("P[%d]: itens[%d] <<- %d\n", (int)pthread_self(), in, item);
+		printf("P: itens[%d] <<- %d\n", in, item);
 		in = (in + 1) % N;
-		sem_post(&mut);
 		sem_post(&cheio);
 	}
 }
@@ -54,21 +48,12 @@ void *consumidor(void *args) {
 }
 
 int main(int argc, char *argv[]) {
-	int i;
 	sem_init(&vazio, 0, N);
 	sem_init(&cheio, 0, 0);
-	sem_init(&mut, 0, 1);
-	
-	pthread_t p_tid[NPROD], c_tid;
-
-	for(i = 0; i < NPROD; i++){
-		pthread_create(&p_tid[i], NULL, produtor, NULL);
-	}
-	
+	pthread_t p_tid, c_tid;
+	pthread_create(&p_tid, NULL, produtor, NULL);
 	pthread_create(&c_tid, NULL, consumidor, NULL);
-	for(i = 0; i < NPROD; i++){
-		pthread_join(p_tid[i], NULL);
-	}
+	pthread_join(p_tid, NULL);
 	pthread_join(c_tid, NULL);
 	return 0;
 }
