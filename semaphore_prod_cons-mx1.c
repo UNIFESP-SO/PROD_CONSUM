fcc971546add3529cc9b0093126de7c7c1e9104a
BrunoFerreira94 <BrunoFerreira94@users.noreply.github.com>
2016-04-06 00:20:10 +0000

    Produtores e consumidores nxm

--- semaphore_prod_cons-mx1.c
+++ semaphore_prod_cons-mx1.c
@@ -9,19 +9,22 @@ Fernando Soares
 #include <stdlib.h>
 #include <unistd.h>
 
+#define NCONS 2
 #define N 10
+
 int itens[N];
 sem_t vazio; // <- N
 sem_t cheio; // <- 0
 int in = 0,  //posicao p/ inserir no buffer itens
     out= 0;  //posicao p/ remover do buffer itens
+int sem[1];
+sem_t mut;
 
 int produz(void) {
 	return rand()%100;
 }
 
 void insere_item(int item){
-	sleep(1);
 	itens[in] = item;
 }
 
@@ -32,10 +35,12 @@ int remove_item(void){
 void *produtor(void *args) {
 	int item;
 	while (1) {
+		sleep(1);
 		sem_wait(&vazio);
 		item = produz();
+		printf("P: itens[%d] ->> %d\n", in, item);
+
 		insere_item(item);
-		printf("P: itens[%d] <<- %d\n", in, item);
 		in = (in + 1) % N;
 		sem_post(&cheio);
 	}
@@ -45,20 +50,31 @@ void *consumidor(void *args) {
 	int item;
 	while (1) {
 		sem_wait(&cheio);
+		sem_wait(&mut);
 		item = remove_item();
-		printf("C: itens[%d] ->> %d\n", out, item);
+		printf("C[%d]: itens[%d] <<- %d\n", (int)pthread_self(), out, item);
 		out = (out + 1) % N;
+		sem_post(&mut);
 		sem_post(&vazio);
 	}
 }
 
 int main(int argc, char *argv[]) {
+	int i;
 	sem_init(&vazio, 0, N);
 	sem_init(&cheio, 0, 0);
-	pthread_t p_tid, c_tid;
+	sem_init(&mut, 0, 1);
+	
+	pthread_t p_tid, c_tid[NCONS];
+
+	for(i = 0; i < NCONS; i++){
+		pthread_create(&c_tid[i], NULL, consumidor, NULL);
+	}
+	
 	pthread_create(&p_tid, NULL, produtor, NULL);
-	pthread_create(&c_tid, NULL, consumidor, NULL);
+	for(i = 0; i < NCONS; i++){
+		pthread_join(c_tid[i], NULL);
+	}
 	pthread_join(p_tid, NULL);
-	pthread_join(c_tid, NULL);
 	return 0;
 }
--- semaphore_prod_cons-mxn.c
+++ semaphore_prod_cons-mxn.c
@@ -9,19 +9,23 @@ Fernando Soares
 #include <stdlib.h>
 #include <unistd.h>
 
+#define NPROD 2
+#define NCONS 2
 #define N 10
+
 int itens[N];
 sem_t vazio; // <- N
 sem_t cheio; // <- 0
 int in = 0,  //posicao p/ inserir no buffer itens
     out= 0;  //posicao p/ remover do buffer itens
+int sem[1];
+sem_t mut, mut1;
 
 int produz(void) {
 	return rand()%100;
 }
 
 void insere_item(int item){
-	sleep(1);
 	itens[in] = item;
 }
 
@@ -32,11 +36,14 @@ int remove_item(void){
 void *produtor(void *args) {
 	int item;
 	while (1) {
+		sleep(1);
 		sem_wait(&vazio);
+		sem_wait(&mut);
 		item = produz();
 		insere_item(item);
-		printf("P: itens[%d] <<- %d\n", in, item);
+		printf("P[%d]: itens[%d] <<- %d\n", (int)pthread_self(), in, item);
 		in = (in + 1) % N;
+		sem_post(&mut);
 		sem_post(&cheio);
 	}
 }
@@ -45,20 +52,38 @@ void *consumidor(void *args) {
 	int item;
 	while (1) {
 		sem_wait(&cheio);
+		sem_wait(&mut1);
 		item = remove_item();
-		printf("C: itens[%d] ->> %d\n", out, item);
+		printf("C[%d]: itens[%d] ->> %d\n",(int)pthread_self(), out, item);
 		out = (out + 1) % N;
+		sem_post(&mut1);
 		sem_post(&vazio);
 	}
 }
 
 int main(int argc, char *argv[]) {
+	int i;
 	sem_init(&vazio, 0, N);
 	sem_init(&cheio, 0, 0);
-	pthread_t p_tid, c_tid;
-	pthread_create(&p_tid, NULL, produtor, NULL);
-	pthread_create(&c_tid, NULL, consumidor, NULL);
-	pthread_join(p_tid, NULL);
-	pthread_join(c_tid, NULL);
+	sem_init(&mut, 0, 1);
+	sem_init(&mut1, 0, 1);
+	
+	pthread_t p_tid[NPROD], c_tid[NCONS];
+
+	for(i = 0; i < NPROD; i++){
+		pthread_create(&p_tid[i], NULL, produtor, NULL);
+	}
+	for(i = 0;i < NCONS; i++){
+		pthread_create(&c_tid[i], NULL, consumidor, NULL);
+
+	}	
+	for(i = 0; i < NPROD; i++){
+		pthread_join(p_tid[i], NULL);
+
+	}
+	for(i = 0; i < NCONS; i++){
+		pthread_join(c_tid[i], NULL);
+
+	}
 	return 0;
 }
